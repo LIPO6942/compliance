2adf7a1247e85f79cc389c7f43973b9c2b79ef46
Lipo <lipo6942@gmail.com>
2025-07-09 18:08:50 +0000

    ⚠ Cross origin request detected from 3001-firebase-studio-1750340242057.

---
+++ src/next.config.ts
@@ -0,0 +1,34 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      "https://3001-firebase-studio-1750340242057.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev",
+    ],
+  },
+};
+
+export default nextConfig;
