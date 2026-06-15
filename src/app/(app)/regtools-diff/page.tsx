6d600fa0eed16b94b023fd042c8913083017547e
Moslem <moslem5017@example.com>
2026-06-15 14:35:03 +0100

    flexbox auto-resizing

--- src/app/(app)/regtools-diff/page.tsx
+++ src/app/(app)/regtools-diff/page.tsx
@@ -6737,7 +6737,7 @@ export default function RegtoolsDiffPage() {
             </div>
 
             {/* Content */}
-            <div className="p-6 flex-1 min-h-0 overflow-hidden flex flex-col md:flex-row gap-6">
+            <div className="p-6 h-[60vh] min-h-[400px] overflow-hidden flex flex-col md:flex-row gap-6">
               {/* Left column: List of delegations and their agencies */}
               <div className="w-full md:w-2/3 flex flex-col gap-4 overflow-hidden h-full min-h-0">
                 <div className="flex justify-between items-center">
